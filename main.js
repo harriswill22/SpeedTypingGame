cef8f17c72e9f5e38ec22d1d7adb330b940bbb8f
Will Harris <Harriswill22@yahoo.com>
2019-02-12 13:37:16 -0500

    creates function for game over

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', init)
+
 let time = 5;
 let score = 0;
 let isPlaying;
@@ -38,3 +40,38 @@ const words = [
     'definition'
 ];
 
+// Initialize Game
+function init() { 
+
+//    Load word from array
+showWord(words)
+// Call countdown every second
+setInterval(countdown,1000)
+//check game status
+setInterval(checkStatus, 50)
+ }
+
+// Pick and show Random Word
+function showWord(words) { 
+    // Generate Random Index
+    const randomIndex = Math.floor(Math.random() * words.length);
+    // Output random word
+    currentWord.innerHTML = words[randomIndex];
+ }
+ function countdown() { 
+
+    if(time > 0) {
+        // Decrement 
+        time--;
+    } else if (time === 0) {
+        isPlaying = false;
+    }
+    timeDisplay.innerHTML = time;
+  }
+
+  function checkStatus() {  
+    if(!isPlaying && time === 0 ) {
+        message.innerHTML ="Game Over!"
+    }
+
+  }
