216a3f93396267233907f61915f705c119f47a65
Will Harris <Harriswill22@yahoo.com>
2019-02-12 13:22:26 -0500

    adds array of words

---
+++ main.js
@@ -0,0 +1,40 @@
+let time = 5;
+let score = 0;
+let isPlaying;
+
+// Don Elements 
+const wordInput = document.querySelector('#word-input')
+const currentWord = document.querySelector('#current-word')
+const scoreDisplay = document.querySelector('#score')
+const timeDisplay = document.querySelector('#time')
+const seconds = document.querySelector('#seconds')
+const message = document.querySelector('#message')
+
+const words = [
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'generate',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition'
+];
+
